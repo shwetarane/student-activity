e7be974d67f132ab54420d04e330d42c700f292d
Shweta <shvetacrane@gmail.com>
2019-11-26 10:33:30 -0600

    made CS a default deptartment

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class UserProfileManager(models.Manager):
     def get_department(self,dept_name=None):
         return super(UserProfileManager,self).get_queryset()\
             .filter(department=dept_name)
-       
+
 class UserManagerNew(models.Manager):
     # Modified by Divya for filtering f_name and l_name from the User model
     def get_name(self,f_name=None, l_name=None):
@@ -24,7 +24,7 @@ class UserProfile(models.Model):
     state = models.CharField(max_length=30, blank=True)
     zip_code = models.IntegerField(default=78758)
     birth_date = models.DateField(null=True, blank=True)
-    department = models.CharField(max_length=20,null=False,blank=False)
+    department = models.CharField(max_length=20,default='Computer Science')
     from_date = models.DateField(default=timezone.now().date())
     to_date = models.DateField(default=timezone.now().date())
     price= models.DecimalField(max_digits=10, decimal_places=2, null=True)
@@ -33,7 +33,7 @@ class UserProfile(models.Model):
 
     # Computer Science manager
     objects = models.Manager()
-    #comp_science = UserProfileManager() 
+    #comp_science = UserProfileManager()
     def __str__(self):
     	return f'{self.user.username} Profile'
 
@@ -41,7 +41,7 @@ class UserProfile(models.Model):
     	super(UserProfile, self).save(*args, **kwargs)
 
     	img = Image.open(self.image.path)
-    	
+
     	if img.height > 300 or img.width > 300:
     		output_size = (300, 300)
     		img.thumbnail(output_size)
