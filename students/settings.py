f1f2f43ad8cf1782a18ff30bb77a2f8508274021
Shweta Rane <shvetacrane@gmail.com>
2020-03-12 20:01:56 -0500

    updated settings.py

--- students/settings.py
+++ students/settings.py
@@ -20,19 +20,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't_186yizknhbf33p@vk194ec3c2l55ytx3lrsd4apvst0zxl5a'
+SECRET_KEY = os.environ.get('STUDENTS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['hidden-reaches-14328.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 # Braintree Payment configuration
 
 BRAINTREE_MERCHANT_ID = '9gyqc4z3ss8w8rv2'
 BRAINTREE_PUBLIC_KEY = '86jjtvmy4d5yrqxj'
-BRAINTREE_PRIVATE_KEY = '44b7682d57d8d9527552cec921513604'
+BRAINTREE_PRIVATE_KEY = os.environ.get('BT_PRIVATE_KEY')
+
 
 from braintree import Configuration, Environment
 Configuration.configure(
@@ -41,8 +42,6 @@ Configuration.configure(
     BRAINTREE_PUBLIC_KEY,
     BRAINTREE_PRIVATE_KEY
 )
-
-
 
 # Application definition
 
@@ -175,8 +174,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('STUDENT_PORTAL_EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('STUDENT_PORTAL_PASSWORD')
 
 # Store the cart in the user Session id
 
